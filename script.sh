aa27c38698ea90f73840023e71a3867c10e22c65
giopera <perigio735@gmail.com>
2024-07-08 18:32:09 +0200

    Finishing the key procedures

--- script.sh
+++ script.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+print_help() {
+    echo "Usage: "
+    echo "ssh-crypt OBJECT { COMMAND | help }"
+    echo ""
+    echo "OBJECT"
+    echo "  key           Manage, add and remove keys in a container"
+    echo "  container     Manage, create, remove and load ssh containers"
+    echo "  host          Manage hosts in configurations"
+    echo ""
+}
+
+print_help_k3(){
+    echo "Usage: "
+    echo "ssh-crypt key <container> { COMMAND | help }"
+    echo ""
+    echo "COMMAND"
+    echo "  generate      Generate a key in a container"
+    echo "  add           Add a key to a container from existing file"
+    echo "  remove        Remove a key from a container"
+    echo "  check         Check if a key exists in the container"
+    echo "  list          Print a list of all the keys in a container"
+    echo ""
+}
+
+containers_file="$HOME/.ssh/containers"
+loaded_file="$HOME/.ssh/loaded_configs"
+containers_dir="$HOME/.local/share/ssh-crypt/containers/"
+container_dir=""
+key_file=""
+
 is_absolute_path() {
     case "$1" in
         /*) return 0 ;;
@@ -14,34 +44,6 @@ process_path() {
     else
         result_process_path="$(pwd)/$path"
     fi
-}
-
-containers_file="$HOME/.ssh/containers"
-loaded_file="$HOME/.ssh/loaded_configs"
-containers_dir="$HOME/.local/share/ssh-crypt/containers/"
-container_dir=""
-key_file=""
-
-print_help() {
-    echo "Usage: "
-    echo "ssh-crypt OBJECT { COMMAND | help }"
-    echo ""
-    echo "OBJECT"
-    echo "  key           Manage, add and remove keys in a container"
-    echo "  container     Manage, create, remove and load ssh containers"
-    echo "  host          Manage hosts in configurations"
-}
-
-print_help_k3(){
-    echo "Usage: "
-    echo "ssh-crypt key <container> { COMMAND | help }"
-    echo ""
-    echo "COMMAND"
-    echo "  generate      Generate a key in a container"
-    echo "  add           Add a key to a container from existing file"
-    echo "  remove        Remove a key from a container"
-    echo "  check         Check if a key exists in the container"
-    echo "  list          Print a list of all the keys in a container"
 }
 
 check_key(){
@@ -59,6 +61,14 @@ check_key(){
 
 
 }
+
+if ! [ -e "$containers_file" ] ; then
+    touch "$containers_file"
+fi
+
+if ! [ -e "$loaded_file" ] ; then
+    touch "$loaded_file"
+fi
 
 if [ -z "$1" ]; then
     print_help
@@ -78,18 +88,45 @@ case $1 in
                 case $3 in
                     g*) # Generate
                         ssh-keygen -f "$container_dir/$4"
+                        return 0
                         ;;
                     a*) # Add
                         process_path "$4"
                         check_key "$result_process_path"
-                        cp "$key_file" "$container_dir"
+                        if [ -n "$key_file" ]; then
+                            cp "$key_file" "$container_dir"
+                            ssh-keygen -f "$container_dir/$key_file" -y > "$container_dir/$key_file.pub"
+                            return 0
+                        else
+                            echo "The key is not a valid key"
+                            return 1
+                        fi
                         ;;
                     r*) # Remove
+                        process_path "$4"
+                        check_key "$result_process_path"
+                        if [ -n "$key_file" ]; then
+                            rm "$key_file"
+                            return 0
+                        else
+                            echo "The key is not a valid key"
+                            return 1
+                        fi
                         ;;
                     c*) # Check
+                        process_path "$4"
+                        check_key "$result_process_path"
+                        if [ -n "$key_file" ]; then
+                            echo "The key exists"
+                            return 0
+                        else
+                            echo "The key doesn't exist"
+                            return 1
+                        fi
                         ;;
                     l*) # List
                         find "$container_dir" -type f -name "*.pub" -exec basename {} \; | sed 's/\.pub$//'
+                        return 1
                         ;;
                     *)
                         print_help_k3
